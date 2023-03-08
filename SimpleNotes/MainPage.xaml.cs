bfd3a176c76d7372d9ed694f6a853a78ff3c3990
Benjamin Mattischek <benjamin.matti@a1.net>
2023-03-08 20:26:40 +0100

    add file

---
+++ SimpleNotes/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using SimpleNotes.ViewModel;
+
+namespace SimpleNotes;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage(MainWindowViewModel viewModel)
+	{
+		InitializeComponent();
+		BindingContext= viewModel;
+	}
+
+	
+}
+
